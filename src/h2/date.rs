d1fe420f911a358a292d869217b27c6c82de43e2
Dorian Niemiec <dorian@ferron.sh>
2026-08-12 17:53:09 +0200

    perf: replace `Mutex` in HTTP/2 date cache with `RwLock`

--- src/h2/date.rs
+++ src/h2/date.rs
@@ -1,11 +1,11 @@
-use std::sync::Mutex;
+use std::sync::RwLock;
 use std::time::UNIX_EPOCH;
 
 use http::HeaderValue;
 
 #[derive(Default)]
 pub(crate) struct DateCache {
-    inner: Mutex<Option<(String, std::time::SystemTime)>>,
+    inner: RwLock<Option<(String, std::time::SystemTime)>>,
 }
 
 impl DateCache {
@@ -18,13 +18,17 @@ impl DateCache {
     #[inline]
     pub fn get_date_header_value(&self) -> Option<HeaderValue> {
         let now = std::time::SystemTime::now();
-        let mut inner = self.inner.lock().ok()?;
+        let mut inner = self.inner.read().ok()?;
         if inner.as_ref().is_none_or(|v| {
             v.1.duration_since(UNIX_EPOCH).ok().map(|d| d.as_secs())
                 != now.duration_since(UNIX_EPOCH).ok().map(|d| d.as_secs())
         }) {
+            drop(inner);
             let value = httpdate::fmt_http_date(now).to_string();
-            inner.replace((value, now));
+            let mut innerw = self.inner.write().ok()?;
+            innerw.replace((value, now));
+            drop(innerw);
+            inner = self.inner.read().ok()?;
         }
         HeaderValue::from_str(inner.as_ref().map(|v| v.0.as_str()).unwrap_or("")).ok()
     }
