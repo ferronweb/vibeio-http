3c86e59d9650bc81f6cee302b7a123efcc9f3a04
Dorian Niemiec <dorian@ferron.sh>
2026-08-14 18:27:31 +0200

    ci: fix h2spec test script syntax error

--- scripts/h2spec.sh
+++ scripts/h2spec.sh
@@ -59,7 +59,7 @@ else
   ./h2spec -h "$HOST" -p "$PORT" -S -j "$JUNIT" $SPEC | tee "$(dirname "$JUNIT")/h2spec.out"
 fi
 status=$?
-if (grep -qF "×" "$(dirname "$JUNIT")/h2spec.out" || grep -qF 'Sends a GOAWAY frame with unknown error code')
+if (grep -qF "×" "$(dirname "$JUNIT")/h2spec.out" || grep -qF 'Sends a GOAWAY frame with unknown error code') \
 && (grep -qF "×" "$(dirname "$JUNIT")/h2spec.out" || grep -vqF 'Sends a GOAWAY frame with unknown error code'); then
   # The flakiness happens on GitHub Actions workflows, ignore the test result
   echo "⚠️ Known flaky test failed: Sends a GOAWAY frame with unknown error code"
